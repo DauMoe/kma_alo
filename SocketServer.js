c329f9f10faeff6b88cd0b971ee897220b1cd2c9
Vinh Nguyen <vinhnn@cmcglobal.vn>
2022-06-17 17:26:59 +0700

    Socket io demo

--- SocketServer.js
+++ SocketServer.js
@@ -8,20 +8,47 @@ server.listen(PORT, function() {
     console.log("RUNNING...");
 });
 
-io.use(function(socket, next) {
-    const username = socket.handshake;
-    // console.log(username, "UN");
-    next();
-});
+// io.use(function(socket, next) {
+//     const username = socket.handshake;
+//     console.log("UN");
+//     next();
+// });
 
-let i = 0;
 
-io.on("connection", function(socket) {
-    console.log("An event is emitted!");
-    socket.on("user_one_two_private_chat", function(msg) {
-        console.log(msg);
-        socket.emit("user_one_two_private_chat", {msg: "go go go "})
-    });
+//Passport + SocketIO: https://socket.io/docs/v4/middlewares/
+// io.use(wrap(session({ secret: "cats" })));
+// io.use(wrap(passport.initialize()));
+// io.use(wrap(passport.session()));
+
+var freetuts = io.of("/");
+//Chi định namespace có tên /freetuts
+freetuts.on("connection", function(socket) {
+  console.log("Một người vừa kết nối.");
+ 
+  //Nhận yêu cầu vào phòng từ clients
+  socket.on("join", function(data) {
+    //THam gia phòng
+    socket.join("freetutsRoom");
+    //Trả lại thông báo cho người vào phòng
+    socket.emit("notification", "Bạn đã tham gia vào phòng");
+ 
+    //Trả lại thông báo cho tất cả người còn lại trong phòng
+    freetuts.to("freetutsRoom").emit("notification", "Một người đã vào phòng.");
+  });
+ 
+  //Nhận yêu cầu rời phòng từ clients
+  socket.on("leave", function(data) {
+    //Rời phòng
+    socket.leave("freetutsRoom");
+    //Trả lại thông báo cho người vào phòng
+    socket.emit("notification", "Bạn đã rời phòng");
+    //Trả lại thông báo cho tất cả người trong phòng
+    freetuts.to("freetutsRoom").emit("notification", "Một người đã rời phòng.");
+  });
+
+  socket.on("sendRoomMsg", function(data) {
+    freetuts.to("freetutsRoom").emit("receivedRoomMsg", data);
+  });
 });
 
 
