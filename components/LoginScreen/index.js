4ad991953aaaa0132db36d71c353960c0a7e0637
Vinh Nguyen <vinhnn@cmcglobal.vn>
2022-06-21 16:22:14 +0700

    Add Login screen

--- components/LoginScreen/index.js
+++ components/LoginScreen/index.js
@@ -4,12 +4,11 @@ import { View, Text, TextInput, KeyboardAvoidingView, TouchableWithoutFeedback, 
 import { Button, AnimatedFAB } from "react-native-paper";
 
 const LoginScreenWrapper = styled.KeyboardAvoidingView`
-    background-color: aliceblue;
     height: ${props => props.height + "px"};
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: darkcyan;
+    background-color: white;
 `;
 
 const LoginFormWrapper = styled.View`
@@ -19,42 +18,53 @@ const LoginFormWrapper = styled.View`
 
 const LoginHeader = styled.Text`
     color       : aliceblue;
-    font-size   : 40px;
-    font-family : "NunitoBlack"
+    font-size   : 60px;
+    font-family : "NunitoBlack";
+    color       : black;
 `;
 
 const LoginTextInput = styled.TextInput`
     background-color: white;
     color           : black;
     position        : relative;
+    font-family     : "NunitoBold";
+    padding         : 10px;
+    border          : solid 2px black;
+    border-radius   : 10px;
+    &::placeholder {
+        color: black
+    }
 `;
 
 const LoginLabel = styled.Text`
-    font-size       : 17px;
+    font-size       : 18px;
     margin-bottom   : 5px;
-    font-weight     : 600;
+    color           : black;
+    font-family     : "NunitoSemiBold";
 `;
 
 const LoginScreen = function(props) {
-    const {width, height} = Dimensions.get("screen");
+    const {width, height} = Dimensions.get("window");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
     return(
         <LoginScreenWrapper behavior={Platform.OS === "ios" ? "padding" : "height"} height={height}>
             <LoginHeader>Login</LoginHeader>
+            <Text style={{color: "black"}}>Normal access account</Text>
             <LoginFormWrapper width={width}>
                 <View>
                     <LoginLabel>Username:</LoginLabel>
-                    <LoginTextInput value={username} onChangeText={setUsername} keyboardType="default"/>
+                    <LoginTextInput placeholderTextColor={'#669999'} value={username} onChangeText={setUsername} placeholder="Username or email"/>
                 </View>
 
                 <View style={{marginTop: 20}}>
                     <LoginLabel>Password:</LoginLabel>
-                    <LoginTextInput value={password} onChangeText={setPassword} keyboardType="default" secureTextEntry={true}/>
+                    <LoginTextInput placeholderTextColor={'#669999'} value={password} onChangeText={setPassword} placeholder="Your password" secureTextEntry={true}/>
                 </View>
 
-                <View>
-                    <Button mode="contained" style={{borderRadius: 10, backgroundColor: "gray"}} loading={true}>Login</Button>
+                <View style={{marginTop: 20}}>
+                    <Button mode="contained" loading={true}>Login</Button>
                 </View>
             </LoginFormWrapper>
             {/* <AnimatedFAB
--- index.js
+++ index.js
@@ -3,14 +3,34 @@
  */
 
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    version: 3,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#3498db',
+      secondary: '#f1c40f',
+      tertiary: '#a1b2c3'
+    },
+    fonts: {
+        ...DefaultTheme.fonts,
+        medium: {fontFamily: 'NunitoBold'},
+        regular:{fontFamily: 'NunitoMedium'},
+        light:{fontFamily: 'NunitoRegular'},
+        thin:{fontFamily: 'NunitoLight'}
+    }
+};
+
 export default function Main() {
     return (
-        <PaperProvider>
+        <PaperProvider theme={theme}>
             <App/>
         </PaperProvider>
     )
