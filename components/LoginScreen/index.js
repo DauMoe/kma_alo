4055f6148326a3ae7211a809ffc4340fa3f8553f
Vinh Nguyen <vinhnn@cmcglobal.vn>
2022-06-21 15:39:22 +0700

    Add Nunito font

--- components/LoginScreen/index.js
+++ components/LoginScreen/index.js
@@ -1,14 +1,69 @@
-import React from "react";;
+import React, { useState } from "react";;
 import styled from "styled-components/native";
-import { View, Text, KeyboardAvoidingView, TouchableWithoutFeedback, Platform } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { View, Text, TextInput, KeyboardAvoidingView, TouchableWithoutFeedback, Platform, Dimensions } from "react-native";
+import { Button, AnimatedFAB } from "react-native-paper";
+
+const LoginScreenWrapper = styled.KeyboardAvoidingView`
+    background-color: aliceblue;
+    height: ${props => props.height + "px"};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: darkcyan;
+`;
+
+const LoginFormWrapper = styled.View`
+    padding: 20px;
+    width: ${props => (props.width - 30) + "px"};
+`;
+
+const LoginHeader = styled.Text`
+    color       : aliceblue;
+    font-size   : 40px;
+    font-family : "NunitoBlack"
+`;
+
+const LoginTextInput = styled.TextInput`
+    background-color: white;
+    color           : black;
+    position        : relative;
+`;
+
+const LoginLabel = styled.Text`
+    font-size       : 17px;
+    margin-bottom   : 5px;
+    font-weight     : 600;
+`;
 
 const LoginScreen = function(props) {
+    const {width, height} = Dimensions.get("screen");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     return(
-        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-            <TextInput mode="flat" label={"Username"} placeholder="Username or email"/>
-            <Button icon="camera" mode="outlined" loading={true} style={{backgroundColor: "green"}}>FFF</Button>
-        </KeyboardAvoidingView>
+        <LoginScreenWrapper behavior={Platform.OS === "ios" ? "padding" : "height"} height={height}>
+            <LoginHeader>Login</LoginHeader>
+            <LoginFormWrapper width={width}>
+                <View>
+                    <LoginLabel>Username:</LoginLabel>
+                    <LoginTextInput value={username} onChangeText={setUsername} keyboardType="default"/>
+                </View>
+
+                <View style={{marginTop: 20}}>
+                    <LoginLabel>Password:</LoginLabel>
+                    <LoginTextInput value={password} onChangeText={setPassword} keyboardType="default" secureTextEntry={true}/>
+                </View>
+
+                <View>
+                    <Button mode="contained" style={{borderRadius: 10, backgroundColor: "gray"}} loading={true}>Login</Button>
+                </View>
+            </LoginFormWrapper>
+            {/* <AnimatedFAB
+                icon={'question'}
+                onPress={() => console.log('Pressed')}
+                visible={true}
+                iconMode={'static'} 
+            />*/}
+        </LoginScreenWrapper>
     );
 };
 
