67d7f73167395b4a62730c550af74716497900e3
Le Hoang <lehuyhoang1111999@gmail.com>
2022-07-07 00:19:18 +0700

    Add TODO

---
+++ DB_Jul_06_2022.sql
@@ -0,0 +1,143 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MariaDB
+ Source Server Type    : MariaDB
+ Source Server Version : 100424
+ Source Host           : localhost:3306
+ Source Schema         : kma_alo
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100424
+ File Encoding         : 65001
+
+ Date: 07/07/2022 00:19:58
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for posts
+-- ----------------------------
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts`  (
+  `POST_ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `AUTHOR_ID` int(10) UNSIGNED NULL DEFAULT NULL,
+  `TITLE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `CONTENT` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `MEDIA_LINKS` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `CREATED_AT` timestamp(0) NOT NULL DEFAULT current_timestamp,
+  `UPDATED_AT` datetime(0) NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`POST_ID`) USING BTREE,
+  INDEX `AUTHOR_ID`(`AUTHOR_ID`) USING BTREE,
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`AUTHOR_ID`) REFERENCES `users` (`UID`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of posts
+-- ----------------------------
+INSERT INTO `posts` VALUES (1, 2, 'SOCIAL MEDIA IS GOOD?', 'If you run this code, you\'ll notice that when you tap \"Go to Details... again\" that it doesn\'t do anything! This is because we are already on the Details route. The navigate function roughly means \"go to this screen\", and if you are already on that screen', NULL, '2022-07-01 21:50:55', '2022-07-01 21:50:55');
+
+-- ----------------------------
+-- Table structure for private_chat
+-- ----------------------------
+DROP TABLE IF EXISTS `private_chat`;
+CREATE TABLE `private_chat`  (
+  `ROOM_CHAT_ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `UID_ONE` int(10) UNSIGNED NULL DEFAULT NULL,
+  `UID_TWO` int(10) UNSIGNED NULL DEFAULT NULL,
+  `CREATED_AT` timestamp(0) NOT NULL DEFAULT current_timestamp,
+  `UPDATED_AT` datetime(0) NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ROOM_CHAT_ID`) USING BTREE,
+  UNIQUE INDEX `LISTEN_EVENT_ID`(`ROOM_CHAT_ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of private_chat
+-- ----------------------------
+INSERT INTO `private_chat` VALUES ('c553e560-fae4-11ec-84ac-215988e5b60c', 2, 3, '2022-07-03 22:28:21', '2022-07-03 22:28:21');
+INSERT INTO `private_chat` VALUES ('caa3e970-fae4-11ec-84ac-215988e5b60c', 2, 4, '2022-07-03 22:28:30', '2022-07-03 22:28:30');
+
+-- ----------------------------
+-- Table structure for private_chat_message
+-- ----------------------------
+DROP TABLE IF EXISTS `private_chat_message`;
+CREATE TABLE `private_chat_message`  (
+  `PRIVATE_CHAT_MSG_ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `CONTENT` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `TYPE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `SENDER_ID` int(10) UNSIGNED NULL DEFAULT NULL,
+  `ROOM_CHAT_ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `CREATED_AT` timestamp(0) NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (`PRIVATE_CHAT_MSG_ID`) USING BTREE,
+  INDEX `ROOM_CHAT_ID`(`ROOM_CHAT_ID`) USING BTREE,
+  CONSTRAINT `private_chat_message_ibfk_1` FOREIGN KEY (`ROOM_CHAT_ID`) REFERENCES `private_chat` (`ROOM_CHAT_ID`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of private_chat_message
+-- ----------------------------
+INSERT INTO `private_chat_message` VALUES ('1b535b12-739c-402c-8869-251e3aa7d8a5', '3', 'TEXT', 0, 'c553e560-fae4-11ec-84ac-215988e5b60c', '2022-07-06 23:53:07');
+INSERT INTO `private_chat_message` VALUES ('f3debb5f-184d-416c-83d6-0cd2bae4f074', '??? WTF bro', 'TEXT', 3, 'c553e560-fae4-11ec-84ac-215988e5b60c', '2022-07-06 23:55:20');
+
+-- ----------------------------
+-- Table structure for relations
+-- ----------------------------
+DROP TABLE IF EXISTS `relations`;
+CREATE TABLE `relations`  (
+  `RELATION_ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `UID_ONE` int(10) UNSIGNED NULL DEFAULT NULL,
+  `UID_TWO` int(10) UNSIGNED NULL DEFAULT NULL,
+  `TYPE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'FOLLOWING, FRIEND, BLOCK',
+  `CREATED_AT` timestamp(0) NOT NULL DEFAULT current_timestamp,
+  `UPDATED_AT` datetime(0) NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`RELATION_ID`) USING BTREE,
+  INDEX `UID_TWO`(`UID_TWO`) USING BTREE,
+  INDEX `UID_ONE`(`UID_ONE`, `UID_TWO`) USING BTREE,
+  CONSTRAINT `relations_ibfk_1` FOREIGN KEY (`UID_ONE`) REFERENCES `users` (`UID`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `relations_ibfk_2` FOREIGN KEY (`UID_TWO`) REFERENCES `users` (`UID`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of relations
+-- ----------------------------
+INSERT INTO `relations` VALUES (1, 2, 3, 'FRIEND', '2022-07-01 21:49:54', '2022-07-01 21:49:54');
+INSERT INTO `relations` VALUES (2, 2, 4, 'FRIEND', '2022-07-01 21:49:54', '2022-07-01 21:49:54');
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `UID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `FIRST_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `LAST_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `USERNAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `MOBILE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `EMAIL` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `PASSWORD` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `LAST_LOGIN` datetime(6) NULL DEFAULT NULL,
+  `INFORMATION` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `FACEBOOK_LINK` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `GOOGLE_LINK` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `EMAIL_CONFIRMED` int(1) NOT NULL DEFAULT 0 COMMENT 'active account if email has confirmed',
+  `ACTIVE_STATUS` int(1) UNSIGNED NULL DEFAULT 0,
+  `LAST_ACTIVE` datetime(6) NULL DEFAULT NULL,
+  `AVATAR_LINK` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `VERIFY_EMAIL_ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `CREATED_AT` timestamp(0) NOT NULL DEFAULT current_timestamp,
+  `UPDATED_AT` datetime(0) NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`UID`) USING BTREE,
+  UNIQUE INDEX `EMAIL`(`EMAIL`) USING BTREE,
+  UNIQUE INDEX `MOBILE`(`USERNAME`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (2, 'Le', 'Hoang', 'daumoe', '0339417291', 'hoangne@gmail.com', '$2b$05$S5W6BHq.5TZ1F8lD3Oazm.LCrl0zUZvR/OYw7E5ADrxMWiCnxNvm.', NULL, NULL, NULL, NULL, 1, 0, NULL, NULL, '', '2022-07-01 21:49:13', '2022-07-01 21:49:13');
+INSERT INTO `users` VALUES (3, 'Nguyen', 'Hien', 'ntth', '098214252', 'hhh@gmail.com', '$05$S5W6BHq.5TZ1F8lD3Oazm.LCrl0zUZvR/OYw7E5ADrxMWiCnxNvm.', NULL, NULL, NULL, NULL, 1, 0, NULL, NULL, '', '2022-07-01 21:49:13', '2022-07-01 21:49:13');
+INSERT INTO `users` VALUES (4, 'John', 'Nguyen', 'john_nguyen', '0982142524', 'hhh1@gmail.com', '$05$S5W6BHq.5TZ1F8lD3Oazm.LCrl0zUZvR/OYw7E5ADrxMWiCnxNvm.', NULL, NULL, NULL, NULL, 1, 0, NULL, NULL, '', '2022-07-01 21:49:13', '2022-07-01 21:49:13');
+
+SET FOREIGN_KEY_CHECKS = 1;
--- components/Chat/PrivateChat/PrivateChatDAO.js
+++ components/Chat/PrivateChat/PrivateChatDAO.js
@@ -4,7 +4,11 @@ const { DB_RESP, DB_ERR } = require("../../../Utils/UtilsFunction");
 const { v4: uuidv4, v1: uuidv3 } = require("uuid");
 
 const FILE_NAME = " - PrivateChatDAO.js";
-
+const MessageType = {
+    TEXT: "TEXT",
+    VIDEO: "VIDEO",
+    VOICE: "VOICE"
+}
 exports.GetAllPrivateChatIDDAO = async(uid) => {
     const FUNC_NAME = "GetAllPrivateChatIDDAO" + FILE_NAME;
     let SQL, SQL_BIND;
@@ -15,7 +19,7 @@ exports.GetAllPrivateChatIDDAO = async(uid) => {
         if (r1.length === 0) {
             return DB_RESP(400, "User ID is not exist");
         }
-        SQL         = "SELECT * FROM PRIVATE_CHAT a, USERS b WHERE (a.UID_ONE = ? AND a.UID_TWO = b.UID) OR (a.UID_TWO = ? AND a.UID_ONE = b.UID)";
+        SQL         = "SELECT * FROM PRIVATE_CHAT a, USERS b, PRIVATE_CHAT_MESSAGE c WHERE ((a.UID_ONE = ? AND a.UID_TWO = b.UID) OR (a.UID_TWO = ? AND a.UID_ONE = b.UID)) AND c.ROOM_CHAT_ID = a.ROOM_CHAT_ID";
         SQL_BIND    = mysql.format(SQL, [uid, uid]);
         const r2    = await query(SQL_BIND);
         return DB_RESP(200, r2);
@@ -51,6 +55,20 @@ exports.CreateNewPrivateChatDAO = async(fromUID, toUID) => {
         await query(SQL_BIND);
         return DB_RESP(200);
     } catch(e) {
+        DB_ERR(FUNC_NAME, SQL_BIND, e.message);
+        return DB_RESP(503, e.message);
+    }
+}
+
+exports.SavePrivateMessageToDBDAO = async(room_name, sender_id, content) => {
+    const FUNC_NAME = "SavePrivateMessageToDBDAO" + FILE_NAME;
+    let SQL, SQL_BIND;
+    try {
+        SQL = "INSERT INTO PRIVATE_CHAT_MESSAGE (PRIVATE_CHAT_MSG_ID, CONTENT, TYPE, SENDER_ID, ROOM_CHAT_ID) VALUES (?, ?, ?, ?, ?)";
+        SQL_BIND = mysql.format(SQL, [uuidv4(), content, MessageType.TEXT, sender_id, room_name]);
+        await query(SQL_BIND);
+        return DB_RESP(200);
+    } catch (e) {
         DB_ERR(FUNC_NAME, SQL_BIND, e.message);
         return DB_RESP(503, e.message);
     }
