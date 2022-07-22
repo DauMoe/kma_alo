5555bc9be2ffc1e45f471626df0c9143173a6f84
Vinh Nguyen <vinhnn@cmcglobal.vn>
2022-07-22 17:24:09 +0700

    Add POSTS

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const { HOST_PORT, HOST_ADDRESS } = require("./Utils/UtilsFunction");
 const { PrivateChatSocket, RoomChatSocket } = require("./components/Chat/ChatSocket");
 const UsersRouter = require("./components/Users/UsersRouter");
 const PrivateChatRouter = require("./components/Chat/PrivateChat/PrivateChatRouter");
+const PostsRouter = require("./components/Posts/PostsRouter");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -39,8 +40,13 @@ app.use(passport.initialize());
 // app.use(passport.session({saveUninitialized: false, secret: "daumoe", cookie: {maxAge: MAX_AGE_SESSION, secure: true}, resave: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/is_ok", (req, resp) => {
+   resp.send("We good");
+});
+
 app.use("/users", UsersRouter);
 app.use("/private_chat", PrivateChatRouter);
+app.use("/posts", PostsRouter);
 
 httpServer.listen(HOST_PORT, function() {
     console.log(`Host IP: '${HOST_ADDRESS}'`);
